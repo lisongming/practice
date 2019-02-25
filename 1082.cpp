3d7188a28342853b6f53e7646de96de34e73b8e1
lisongming <939200869@qq.com>
2019-02-25 13:11:31 +0800

    Create 1082.cpp

---
+++ 1082.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int i,t,n,j;
+	scanf("%d",&t);
+	for(i=1;i<=t;i++){
+		scanf("%d",&n);
+		for(j=1;j<=n;j++){
+			if(j%7==0||j%10==7||j/10%10==7||j/100%10==7||j/1000%10==7){
+			    printf("%d ",j);
+			}
+			}
+			printf("\n");
+		}
+	return 0;
+}
+
