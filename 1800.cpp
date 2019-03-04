4d19e3db25d8edcbb1f258008e5e4c40eec0cf70
lisongming <939200869@qq.com>
2019-03-04 15:48:42 +0800

    Create 1800.cpp

---
+++ 1800.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	while(scanf("%d",&n)!=EOF){
+		for(int i=1;i<=n;i++){
+		printf("年轻人要少水群多刷题！\n");
+	}printf("\n");
+	}
+	return 0;
+}
+
