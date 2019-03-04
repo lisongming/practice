634555f1b97509195ac07ff839146bfba7bed1b6
lisongming <939200869@qq.com>
2019-03-04 14:39:10 +0800

    Update 全排列.cpp

--- 全排列.cpp
+++ 全排列.cpp
@@ -1,4 +1,4 @@
-#includ<bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 const int N=10;
 bool used[N];
@@ -28,5 +28,4 @@ int main()
 	int arr[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	memset(used, 0, sizeof(used));
 	perm(arr, 0, 2);
-}
 }
