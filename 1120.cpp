3b167faacc0fc014446288d5a0524f786a0b168e
lisongming <939200869@qq.com>
2019-03-17 11:10:43 +0800

    Update 1120.cpp

--- 1120.cpp
+++ 1120.cpp
@@ -1,25 +1,6 @@
-#include<stdio.h>
-int	MinIndex(int a[],int n);
-int	MaxIndex(int a[],int n);
-void PrintArr(int a[],int n);
-int main(){
-  int a[10],i,j,n;
-  int max_flag,min_flag;
-   scanf("%d",&n);
-   for(i=0;i<n;i++){
-   	scanf("%d",&a[i]);
-   }
-    min_flag= MinIndex(a,n);
-  j=a[0];
-  a[0]=a[min_flag];
-  a[min_flag]=j;
-   max_flag= MaxIndex(a,n);
-  j=a[max_flag];
-  a[max_flag]=a[n-1];
-  a[n-1]=j;
-  PrintArr(a,n);
-  return 0;
-}
+#include<bits/stdc++.h>
+using namespace std;
+
 int MinIndex(int a[],int n){
   	int i ,min=a[0],flag;
 	  for(i=0;i<n;i++){
@@ -47,5 +28,24 @@ void PrintArr(int a[],int n)
 		printf("%d ",a[i]);
 	printf("%d",a[n-1]);
 	return;
+}
+int main()
+{
+	int a[10],i,j,n;
+	int max_flag,min_flag;
+	scanf("%d",&n);
+	for(i=0;i<n;i++){
+		scanf("%d",&a[i]);
+	}
+	min_flag=MinIndex(a,n);
+	j=a[0];
+	a[0]=a[min_flag];
+	a[min_flag]=j;
+	max_flag=MaxIndex(a,n);
+	j=a[max_flag];
+	a[max_flag]=a[n-1];
+	a[n-1]=j;
+	PrintArr(a,n);
+	return 0;
 }
 
