1699a2d292d8f619f864f6fae5c3b280829c789c
lisongming <939200869@qq.com>
2019-03-17 11:09:47 +0800

    Create 2514.cpp

---
+++ 2514.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n,s,i;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++){
+		scanf("%d",&s);
+	}
+	for(i=1;i<=n;i++){
+		if(s%2!=0){
+			printf("%d ",s);
+		}
+		else if(s%2==0){
+			printf("%d ",s);
+	}
+}
+	return 0;
+}
+
