86cd5ba82653249ebaa9d15a7c984cce90f0ff36
lisongming <939200869@qq.com>
2019-03-17 11:10:04 +0800

    Update 2330.cpp

--- 2330.cpp
+++ 2330.cpp
@@ -1,19 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+ 
 int main()
 {
-	int a,b,c,d,s;
-	double x;
-	scanf("%d %d %d %d",&a,&b,&c,&d);
-	s=100-a*5-b*12-c*15;
-	x=s*0.3+d*0.7;
-	if(x<60){
-		printf("Fail");
-	}
-	else if(x>=60){
-		printf("Pass");
-	}
-	return 0;
+    int a,b,c,d,s;
+    double x;
+    scanf("%d %d %d %d",&a,&b,&c,&d);
+    s=100-(a*5)-(b*12)-(c*15);
+    x=s*0.3+d*0.7;
+    if(x<60){
+        printf("Fail");
+    }
+    else{
+        printf("Pass");
+    }
+    return 0;
 }
-
