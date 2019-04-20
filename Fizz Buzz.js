eefed053413e6d799ab7d522ad16e1cae96aaaef
lisongming <939200869@qq.com>
2019-04-20 16:24:22 +0800

    Create Fizz Buzz.js

---
+++ Fizz Buzz.js
@@ -0,0 +1,19 @@
+var fizzBuzz=function(n){
+    var result=[];
+    for(var i=1;i<=n;i++){
+        if(i%3==0&&i%5==0){
+            result.push('FizzBuzz');
+        }
+        else if(i%5==0){
+            result.push('Buzz');
+        }
+        else if(i%3==0){
+            result.push('Fizz'); 
+        }
+        else{
+            result.push(""+i);
+        } 
+    }  
+    return result;
+};
+
