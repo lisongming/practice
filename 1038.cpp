66af2fae2d7f42db0f7f04133c74b5d4086ccb73
lisongming <939200869@qq.com>
2019-04-20 16:22:38 +0800

    Create 1038.cpp

---
+++ 1038.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int a,b,c,max;
+	scanf("%d%d%d",&a,&b,&c);
+	max=a;
+	if(abs(b)>abs(max)){
+		max=b;
+	}
+	if(abs(c)>abs(max)){
+		max=c;
+	}
+	printf("%d",max);
+	return 0;
+}
+
