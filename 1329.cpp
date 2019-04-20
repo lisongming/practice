8bac8f901b60f824b89591acaca029e2363bb161
lisongming <939200869@qq.com>
2019-04-20 16:22:54 +0800

    Create 1329.cpp

---
+++ 1329.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n,s,a;
+	double x;
+	while(scanf("%d",&n),n!=0){
+		s=n;
+		x=0;
+		while(s>=3){
+		a=s/3;
+		a+=x;
+	}
+	//if(s==2){
+		//x=x+1;
+	//}
+	printf("%lf",x);
+	}
+	return 0;
+}
+
