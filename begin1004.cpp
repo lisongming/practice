18d41a2c891be55abde85a1dd3d9c78eb6e4eaed
lisongming <939200869@qq.com>
2019-05-13 23:52:24 +0800

    Create begin1004.cpp

---
+++ begin1004.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	char s;
+	int i=0;
+	scanf("%c",&s);
+	printf("%c\n",s);
+	while(i<3){
+		printf("%c",s);
+		i++;	
+	}
+	printf("\n");
+	i=0;
+	while(i<5){
+		printf("%c",s);
+		i++;
+	}
+	return 0;
+}
+
