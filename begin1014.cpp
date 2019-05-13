3e3c6c8c2c966d5cb25be3e9384d3c8a51365057
lisongming <939200869@qq.com>
2019-05-13 23:52:53 +0800

    Create begin1014.cpp

---
+++ begin1014.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	double r,a,b,c;
+	scanf("%lf",&r);
+	a=r*2;
+	b=3.1415926*r*2;
+	c=3.1415926*r*r;
+	printf("%.4lf %.4lf %.4lf",a,b,c);
+	return 0;
+}
+
